10785abd66f998cade7e2f14884b91dc36d69928
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2017-11-05 20:42:57 +0530

    Update

--- magento-2.1/clean.sh
+++ magento-2.1/clean.sh
@@ -3,12 +3,11 @@
 rm -fr var/cache/*
 rm -fr var/page_cache/*
 rm -fr var/generation/*
-rm -fr var/di/*
 rm -fr var/view_processed/*
 rm -fr pub/static/*
 
-bin/magento setup:di:compile
-bin/magento setup:upgrade
+#bin/magento setup:di:compile
+#bin/magento setup:upgrade
 bin/magento cache:clean
 bin/magento cache:flush
 #bin/magento setup:static-content:deploy
