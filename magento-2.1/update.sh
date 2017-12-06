95ec457bda7f41d05372911305672d2c30024423
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2017-12-06 12:43:17 +0530

    Update

--- magento-2.1/update.sh
+++ magento-2.1/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bin/magento maintenance:enable
+
 rm -fr var/cache/*
 rm -fr var/page_cache/*
 rm -fr var/generation/*
@@ -11,4 +13,7 @@ bin/magento setup:di:compile
 bin/magento setup:upgrade
 bin/magento cache:clean
 bin/magento cache:flush
-#bin/magento setup:static-content:deploy
+bin/magento setup:static-content:deploy
+#bin/magento dev:tests:run unit
+
+bin/magento maintenance:disable
